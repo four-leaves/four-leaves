920f8e603fdfeaf3851482229dfe61da4490bd3a
Franz <evenhold@gmail.com>
2019-07-24 13:44:01 -0500

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,18 @@
+echo "|-------------------------|
+|   FOUR LEAVES compiler  |
+|-------------------------|
+
+"
+ls | grep *.tex 
+echo "
+"
+echo "Please enter your  tex name"
+read filename
+
 rm -f *.log *.aux *.fdb_latexmk *.out *.toc
-pdflatex four-leaves.tex -shell-scape -synctex=1
-pdflatex four-leaves.tex -shell-scape -synctex=1
-pdflatex four-leaves.tex -shell-scape -synctex=1
+pdflatex $filename -shell-scape -synctex=1
+pdflatex $filename -shell-scape -synctex=1
+pdflatex $filename -shell-scape -synctex=1
 rm -f *.log *.aux *.fdb_latexmk *.out *.toc
 
 # https://tex.stackexchange.com/questions/342656/have-kile-detect-whether-to-use-pdflatex-or-xelatex
